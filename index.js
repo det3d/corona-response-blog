7cbeff2de645bcb11b243a76704aefd684c0ed97
MakalakaMac <makalakamac@MakalakaMacsAir.gha.chartermi.net>
2020-04-14 18:38:59 -0400

    tutorial with basic routes

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const express = require(express);
+const express = require('express');
 const app = express();
 const path = require('path');
+
+//app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('APP LISENTING ON port 3000');
+//app.use('/', express.static(__dirname + 'public'));
+//app.use('/', express.static(__dirname + 'public'));
+
+//console.log(path.resolve(__dirname, 'pages/index.html'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
+app.listen(4000, () => {
+  console.log('APP LISTENING ON port 4000');
 });
