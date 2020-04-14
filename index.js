e9745ebf65b038614a2ae39f9d2afb36bdd8f6d2
MakalakaMac <makalakamac@MakalakaMacsAir.gha.chartermi.net>
2020-04-14 13:02:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require(express);
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('APP LISENTING ON port 3000');
+});
